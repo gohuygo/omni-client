ccb75d9892b1d50788c5520b6ebe6b6f65277210
gohuygo <huy@gohuygo.com>
2018-12-12 18:13:30 -0800

    Setup router and home container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route }  from 'react-router-dom'
+
+import Home from './containers/Home'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>Hello world</p>
-      </div>
+      <Router>
+        <div>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/address' component={Home} />
+        </div>
+      </Router>
     );
   }
 }
