711ecdf0e0c2315812937fbf57c376560c412917
gohuygo <huy@gohuygo.com>
2018-12-12 18:40:34 -0800

    Setup Navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route }  from 'react-router-dom'
 
 import Home from './containers/Home'
+import Navbar from './components/Navbar'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <Navbar />
+
           <Route exact path='/' component={Home} />
           <Route exact path='/address' component={Home} />
         </div>
